6b6cc6695c27ee4639b9a1dd4d36efce19de0712
YoungPhilGim <youngphil.gim@kasset.co.kr>
2022-02-07 08:40:07 +0900

    fixed

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,12 @@
 <script>
-	export let name;
+import Login from './Login.svelte';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<section class="container-fluid">
+  <div class="row justify-content-center">
+		<div class="py-3 bg-light display-4 text-center">Travel Packing Checklist</div>
+    <div class="col-sm-6 col-md-4 col-xl-3 pt-4">
+      <Login />
+    </div>
+  </div>
+</section>
