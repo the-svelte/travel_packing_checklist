e56fec5f9167ce7310670133c1596ed1fe12fa93
YoungPhilGim <youngphil.gim@kasset.co.kr>
2022-02-07 17:05:18 +0900

    fixed

---
+++ src/Item.svelte
@@ -0,0 +1,44 @@
+<script>
+  import {blurOnKey} from './util';
+
+  export let item;
+
+  let editing = false;
+
+  
+</script>
+
+<li class="list-group-item">
+  <div class="d-flex justify-content-between">
+    <div class="form-check">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckDefault"
+        bind:checked={item.packed}
+      >
+      {#if editing}
+        <input
+          class="form-input"
+          bind:value={item.name}
+          on:blur={() => editing = false}
+          on:keydown={blurOnKey}
+        />
+      {:else}
+        <label
+          class:decoration={item.packed}
+          class="form-check-label"
+          for="flexCheckDefault"
+          on:click={() => editing = true}
+        >{item.name}</label>
+      {/if}
+    </div>
+    <div class="">
+      <button
+        type="button"
+        class="btn btn-primary btn-sm"
+      >Confirm identity</button>
+    </div>
+  </div>
+</li>
